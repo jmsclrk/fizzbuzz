c7194b5d2c657a01f53bf8d4ab877e0b38688981
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-12 13:39:55 +0100

    add test for buzz and other number

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,5 @@
 def fizzbuzz(number)
-  'fizz'
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number % 5 == 0
+  return number.to_s
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,3 +4,24 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "19" when passed 19' do
+    expect(fizzbuzz(19)).to eq '19'
+  end
+end
+
+=begin
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
+
+=end
