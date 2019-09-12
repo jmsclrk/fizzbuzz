e9ddab1c10b58c5b0189995c265580da90c958ce
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-12 14:10:56 +0100

    Add fizzbuzz method to Integer class

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,25 @@
 require 'fizzbuzz'
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+
+describe '.fizzbuzz' do
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
   end
 end
 
-describe 'fizzbuzz' do
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+describe '.fizzbuzz' do
+  it 'returns "fizz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
   end
 end
 
-describe 'fizzbuzz' do
-  it 'returns "19" when passed 19' do
-    expect(fizzbuzz(19)).to eq 19
+describe '.fizzbuzz' do
+  it 'returns "fizz" for the number 19' do
+    expect(19.fizzbuzz).to eq 19
   end
 end
 
-describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+describe '.fizzbuzz' do
+  it 'returns "fizz" for the number 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
 end
